cdbbb5a3f43ddd96e7c611b85ce5f1b85a058775
RuneRask <r.rask@hotmail.com>
2017-05-03 13:22:45 +0200

    Minor changes to WeatherUpdater

--- WeatherAarhusGroup02/app/src/main/java/com/example/group02/weatheraarhusgroup02/Services/WeatherUpdater.java
+++ WeatherAarhusGroup02/app/src/main/java/com/example/group02/weatheraarhusgroup02/Services/WeatherUpdater.java
@@ -2,15 +2,27 @@ package com.example.group02.weatheraarhusgroup02.Services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
+import com.example.group02.weatheraarhusgroup02.Model.Weather;
+
 public class WeatherUpdater extends Service {
+
+    private final IBinder mBinder = new LocalBinder();
+
     public WeatherUpdater() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+    public class LocalBinder extends Binder {
+        WeatherUpdater getService() {
+            return WeatherUpdater.this;
+        }
     }
 }
