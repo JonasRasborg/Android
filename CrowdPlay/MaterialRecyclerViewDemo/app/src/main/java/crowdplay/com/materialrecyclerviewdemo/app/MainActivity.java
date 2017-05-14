8ff6c0cb2b5b036fa5b6f76caf6087713676b368
DESKTOP-VTD19TK\rrask <r.rask@hotmail.com>
2017-05-14 16:56:00 +0200

    Few changes

--- CrowdPlay/MaterialRecyclerViewDemo/app/src/main/java/crowdplay/com/materialrecyclerviewdemo/app/MainActivity.java
+++ CrowdPlay/MaterialRecyclerViewDemo/app/src/main/java/crowdplay/com/materialrecyclerviewdemo/app/MainActivity.java
@@ -1,15 +1,19 @@
 package crowdplay.com.materialrecyclerviewdemo.app;
 
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.animation.OvershootInterpolator;
 
 import crowdplay.com.materialrecyclerviewdemo.R;
 import crowdplay.com.materialrecyclerviewdemo.adaptor.RecyclerAdapter;
 import crowdplay.com.materialrecyclerviewdemo.model.Track;
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
         toolbar.setTitle("CrowdPlay");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimaryLight));
+
         toolbar.inflateMenu(R.menu.menu_main);
 
         setUpRecyclerView();
@@ -39,7 +46,12 @@ public class MainActivity extends AppCompatActivity {
         mLinearLayoutManagerVertical.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(mLinearLayoutManagerVertical);
 
-        recyclerView.setItemAnimator(new DefaultItemAnimator()); // Even if we dont use it then also our items shows default animation. #Check Docs
+        recyclerView.getItemAnimator().setChangeDuration(400);
+        recyclerView.getItemAnimator().setMoveDuration(300);
+        recyclerView.getItemAnimator().setRemoveDuration(200);
+        recyclerView.getItemAnimator().setAddDuration(300);
+
+        //recyclerView.setItemAnimator(new DefaultItemAnimator()); // Even if we dont use it then also our items shows default animation. #Check Docs
     }
 
 }
