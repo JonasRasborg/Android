939ff3c90cd030eb5529e3293ff883df7c628ee4
Jonas R. Hartogsohn <Jonas.Rasborg@Gmail.com>
2017-05-17 15:11:04 +0200

    Klasser tilføjet

--- CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/GuestActivity.java
+++ CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/GuestActivity.java
@@ -1,15 +1,36 @@
 package cpmusic.com.crowdplay.activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 import cpmusic.com.crowdplay.R;
 
 public class GuestActivity extends AppCompatActivity {
 
+    EditText editSearch;
+    FloatingActionButton fabSearch;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
+
+        editSearch = (EditText)findViewById(R.id.editSearch);
+        fabSearch = (FloatingActionButton)findViewById(R.id.fabSearch);
+        listView = (ListView)findViewById(R.id.listView);
+
+        fabSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/util/APIConnector.java
+++ CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/util/APIConnector.java
@@ -1,8 +1,56 @@
 package cpmusic.com.crowdplay.util;
 
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import cpmusic.com.crowdplay.model.spotifyModel.Example;
+
 /**
  * Created by rrask on 17-05-2017.
  */
 
 public class APIConnector {
+
+    RequestQueue queue;
+    String webResponse;
+    Example data;
+
+    public void Search(String artist, String title, Context c)
+    {
+        String url = "https://api.spotify.com/v1/search?q=" + artist + "+" + title + "&type=artist,track";
+
+        sendRequest(url, c);
+    }
+
+
+    public void sendRequest(String url, Context c) {
+        //send request using Volley
+        if (queue == null) {
+            queue = Volley.newRequestQueue(c);
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        webResponse = response;
+                        data = JSONParser.parseSearchWithJsonParser(webResponse);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        queue.add(stringRequest);
+
+    }
 }
