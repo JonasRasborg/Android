f37fd28210724e9c53356b62dbfc562cac10127f
Rune Rask <r.rask@hotmail.com>
2017-05-17 23:28:51 +0200

    Firebase connector almost done - problems with databaseref

--- CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/DJActivity.java
+++ CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/DJActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -15,33 +17,39 @@ import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
+import cpmusic.com.crowdplay.util.FirebaseConnector;
+
 
 import cpmusic.com.crowdplay.R;
+import cpmusic.com.crowdplay.model.firebaseModel.Tracks;
 
 public class DJActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
     private static final String TAG = DJActivity.class.getSimpleName();
-
     private static final String CLIENT_ID = "1c078fda78264a8e8372cc576a259a1a";
-
     private static final String REDIRECT_URI = "crowdplay://callback";
-
     private static final int REQUEST_CODE = 1337;
 
+
     private Player mPlayer;
+
+    private Tracks tracks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dj);
 
-        Log.i("DJActivity","OnCreate");
+        Log.i(TAG,"OnCreate");
+
+
 
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+        
     }
 
     @Override
@@ -72,7 +80,7 @@ public class DJActivity extends AppCompatActivity implements SpotifyPlayer.Notif
 
     @Override
     public void onLoggedIn() {
-        Log.d("TAG", "User logged in");
+        Log.d(TAG, "User logged in");
 
         mPlayer.playUri(null, "spotify:track:33frkbGSt6bbfj2Nqo9i1p", 0, 0);
         mPlayer.setRepeat(null, true);
@@ -80,27 +88,27 @@ public class DJActivity extends AppCompatActivity implements SpotifyPlayer.Notif
 
     @Override
     public void onLoggedOut() {
-        Log.d("TAG", "User logged out");
+        Log.d(TAG, "User logged out");
     }
 
     @Override
     public void onLoginFailed(Error error) {
-        Log.d("TAG", "Login failed");
+        Log.d(TAG, "Login failed");
     }
 
     @Override
     public void onTemporaryError() {
-        Log.d("TAG", "Temporary error occurred");
+        Log.d(TAG, "Temporary error occurred");
     }
 
     @Override
     public void onConnectionMessage(String s) {
-        Log.d("TAG", "Received connection message: " + s);
+        Log.d(TAG, "Received connection message: " + s);
     }
 
     @Override
     public void onPlaybackEvent(PlayerEvent playerEvent) {
-        Log.d("TAG", "Playback event received: " + playerEvent.name());
+        Log.d(TAG, "Playback event received: " + playerEvent.name());
         switch (playerEvent) {
             // Handle event type as necessary
             default:
@@ -110,7 +118,7 @@ public class DJActivity extends AppCompatActivity implements SpotifyPlayer.Notif
 
     @Override
     public void onPlaybackError(Error error) {
-        Log.d("TAG", "Playback error received: " + error.name());
+        Log.d(TAG, "Playback error received: " + error.name());
         switch (error) {
             // Handle error type as necessary
             default:
--- CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/util/FirebaseConnector.java
+++ CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/util/FirebaseConnector.java
@@ -1,8 +1,34 @@
 package cpmusic.com.crowdplay.util;
 
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import cpmusic.com.crowdplay.model.firebaseModel.Track;
+import cpmusic.com.crowdplay.model.firebaseModel.Tracks;
+
 /**
  * Created by rrask on 17-05-2017.
  */
 
 public class FirebaseConnector {
+
+    private String FirebaseRoot = "https://crowdplay-1191b.firebaseio.com/";
+
+    DatabaseReference mRootRef;
+
+    public FirebaseConnector(DatabaseReference db){
+
+
+        mRootRef = db;
+    }
+
+    public void putNewTrack(Track track){
+        mRootRef.push().setValue(track);
+    }
+
 }
