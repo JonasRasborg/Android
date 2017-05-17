5ddde96a3730cea8c1b06c98ed3233d9cc8ff881
Rune Rask <r.rask@hotmail.com>
2017-05-17 15:45:46 +0200

    firebaseDTO

--- CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/DJActivity.java
+++ CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/DJActivity.java
@@ -1,15 +1,24 @@
 package cpmusic.com.crowdplay.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cpmusic.com.crowdplay.R;
+import cpmusic.com.crowdplay.services.MusicPlayerService;
 
 public class DJActivity extends AppCompatActivity {
+
+    MusicPlayerService musicPlayerService;
+    Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dj);
+
+        //serviceIntent = new Intent(MainActivity)
+
+
     }
 }
---
+++ CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/model/firebaseModel/Test.java
@@ -0,0 +1,8 @@
+package cpmusic.com.crowdplay.model.firebaseModel;
+
+/**
+ * Created by rrask on 17-05-2017.
+ */
+
+public class Test {
+}
