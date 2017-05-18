3a6722ff4d72a1cbc20dc3fda0322d468147cb09
DESKTOP-P4ABAF6\ander <anderswiggers41@gmail.com>
2017-05-18 11:58:41 +0200

    PartyFinderActivity Updated ..

--- CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/PartyFinderActivity.java
+++ CrowdPlayMappe/CrowdPlay/app/src/main/java/cpmusic/com/crowdplay/activities/PartyFinderActivity.java
@@ -1,13 +1,23 @@
 package cpmusic.com.crowdplay.activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import cpmusic.com.crowdplay.R;
@@ -15,6 +25,13 @@ import cpmusic.com.crowdplay.R;
 public class PartyFinderActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LatLng navitas = new LatLng(56.158897, 10.213706);
+    LatLng party1 = new LatLng(56.1587, 10.213);
+    static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION=0;
+    int minTime = 5000; // millisecs
+    int minDistance = 5; // meters
+    private Location userlocation;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +41,132 @@ public class PartyFinderActivity extends FragmentActivity implements OnMapReadyC
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Check if app has permission to use Location (if FINE is permittes, so is COARSE)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // If not, ask for it
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            // onRequestPermissionsResult is automatically invoked now
+        }
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // When map async call returns (map is built on screen)
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // put user location on map
+        PutMylocationOnMap();
+
+        // Start listening for user location updates - if changed enough map is updated
+        ListenForUserLocationUpdates();
+
+        // Add parties to map .... should be Firebase code here
+        mMap.addMarker(new MarkerOptions().position(navitas).title("Navitas Party"));
+        mMap.addMarker(new MarkerOptions().position(party1).title("Pølse Party"));
+
+
+        // Listener for marker (Pin) click
+       /* mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                Intent intent = new Intent(MapsActivity.this,PartyActivity.class);
+                Log.d("Title: ",marker.getTitle());
+                intent.putExtra("PartyTitle",marker.getTitle());
+                startActivity(intent);
+                return true;
+            }
+        });*/
+
     }
+
+    public void ListenForUserLocationUpdates()
+    {
+        if (locationManager == null) {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        }
+
+        if (locationManager != null) {
+            try {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, locationListener);
+
+            } catch (SecurityException ex) {
+                //TODO: user have disabled location permission - need to validate this permission for newer versions
+            }
+        } else {
+
+        }
+    }
+
+    public void PutMylocationOnMap()
+    {
+        // Cheking if app has permission to get users location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // If not, ask for it
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            // onRequestPermissionsResult is automatically invoked now
+
+        }
+        else {
+            mMap.setMyLocationEnabled(true);
+        }
+
+
+    }
+
+    // Invokes når der kommer svar på requestPermissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    PutMylocationOnMap();
+
+
+                } else {
+
+                    Toast.makeText(this, "Cant use the app without allowing permissions to location", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    // Lytter på når GPS location ændres (væsentligt)
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            // Update user location
+            LatLng userLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng,15));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 }
